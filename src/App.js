3591b8e3e0ed6919835fdfb9961b28730da71193
Alejandro Carrizo <alelacroa@gmail.com>
2021-11-14 03:11:21 -0300

    Clase 5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import iphone from './images/image.jpg'
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <img src={iphone} />
+      <h2>Iphone 13</h2>
+      <h2>Selecciona la cantidad que deseas comprar:</h2>
+      <h1 className={count > 0 ? "positive" : null}>
+        {count}
+      </h1>
+      <div className="button__wrapper">
+        <button disabled={count < 1} onClick={() => setCount(count - 1)}>-</button>
+        <button disabled={count > 9} onClick={() => setCount(count + 1)}>+</button>
+      </div>
+      <h2 className={count < 10 ? "ocultar" : null}>No puedes comprar mas de 10 unidades</h2>
     </div>
   );
 }
